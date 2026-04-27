6cd91f5998d2e6ea46417f053f90ec4bf98235a3
Isaiah Raymond <verscial77@gmail.com>
2026-04-27 16:37:46 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+
+let escapes = 0;
+
+
+const btn = document.getElementById('run-away-btn');
+const escapeLabel = document.getElementById('escape-score');
+
+
+btn.addEventListener('mouseover', function() {
+
+
+const x - Math.random() * (window.innerWidth - btn.clientWidth);
+const y = Math.random() * (window.innerHeight - btn.clientHeight);
+
+
+btn.style.left = x + 'px';
+btn.style.top = y + 'px';
+
+
+  escapes  = escapes + 1;
+  escapeLabel.innerText = escapes;
+});
+
+
+btn.addEventListener('click', function() {
+  alert("NO WAY! You Actually Caught It!");
+  escapes = 0;
+  escapeLabel.innerText = escapes;
+});
