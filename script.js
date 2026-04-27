28bfd88996444cb75f3c51362f6afe036afff726
Isaiah Raymond <verscial77@gmail.com>
2026-04-27 17:26:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 let escapes = 0;
+let currentSize = 100;
 const btn = document.getElementById('run-away-btn');
 const escapeLabel = document.getElementById('escape-score');
 
@@ -19,9 +20,16 @@ btn.style.top = y + 'px';
   escapeLabel.innerText = escapes;
 });
 
+if (currentSize > 20) {
+  currentSize = currentSize - 2;
+  btn.style.transform = `scale(${currentSize / 100})`;
+}
+});
 
 btn.addEventListener('click', function() {
   alert("NO WAY! You Actually Caught It!");
   escapes = 0;
+  currentSize = 100;
+  btn.style.transform = 'scale(1)';
   escapeLabel.innerText = escapes;
 });
